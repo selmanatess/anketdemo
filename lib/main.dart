d8c9f1c4208eee5f245d8c9082c69022d639fe64
Selman ATEŞ <selmanates007@gmail.com>
2022-09-02 20:55:06 +0300

    Commit

---
+++ lib/main.dart
@@ -0,0 +1,48 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore_web/cloud_firestore_web.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+      ),
+      home:
+    );
+  }
+}
+
+class Anket {
+  late String isim;
+  late int oy;
+  // ignore: prefer_typing_uninitialized_variables
+  null
+
+  late DocumentReference reference;
+  Anket.fromMap(Map<String, dynamic> map,{this.reference} )
+      : assert(map["isim"] != null),
+        assert(map["oy"] != null),
+        isim = map["isim"],
+        oy = map["oy"];
+  Anket.fromSnapshot(DocumentSnapshot snapshot)
+      :this.fromMap(snapshot.data,snapshot.reference)
+}
